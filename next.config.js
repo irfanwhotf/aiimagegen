1a05c7672522193e085b8d3c2a39975202f56e19
irfanwhotf <mohammediirfan2006@gmail.com>
2025-01-20 19:39:01 +0530

    Add Cloudflare Pages configuration files

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
         hostname: 'oaidalleapiprodscus.blob.core.windows.net',
-      }
+      },
     ],
+  },
+  experimental: {
+    appDir: true,
   },
 }
 
